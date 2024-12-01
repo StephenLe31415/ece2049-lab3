13895826087097c1a5eaf09b2b2f238083eb2c46
rye-hogan <rhogan@wpi.edu>
2024-12-01 12:49:09 -0500

    Testing adc12_tempSensor

--- main.c
+++ main.c
@@ -2,20 +2,55 @@
 #include <msp430.h>
 #include "peripherals.h"
 #include "functions.h"
-
+/*----------------------------------------------------------------------------------- */
+// Temperature Sensor Calibration = Reading at 30 degrees C is stored at addr 1A1Ah
+// See end of datasheet for TLV table memory mapping
+#define CALADC12_15V_30C *((unsigned int *)0x1A1A)
+// Temperature Sensor Calibration = Reading at 85 degrees C is stored at addr 1A1Ch
+//See device datasheet for TLV table memory mapping
+#define CALADC12_15V_85C *((unsigned int *)0x1A1C)
+unsigned int in_temp;
+/*----------------------------------------------------------------------------------- */
 volatile long unsigned int global_counter = 16416000;
 
 #pragma vector=TIMER2_A0_VECTOR //What does this do? No one knows...
 __interrupt void timer_a2() {
   global_counter++;
+  Graphics_clearDisplay(&g_sContext); // Clear the display
+  Graphics_flushBuffer(&g_sContext);
 }
 
 void main() {
+
+  /*----------------------------------------------------------------------------------- */
+  volatile float temperatureDegC;
+  volatile float temperatureDegF;
+  volatile float degC_per_bit;
+  volatile unsigned int bits30, bits85;
+  /*----------------------------------------------------------------------------------- */
+
   WDTCTL = WDTPW | WDTHOLD;    // Stop watchdog timer. Always need to stop this!!
   // You can then configure it properly, if desired
 
   // Global interrupt enable
   _BIS_SR(GIE);
+
+  /*----------------------------------------------------------------------------------- */
+  REFCTL0 &= ~REFMSTR; // Reset REFMSTR to hand over control of
+  // internal reference voltages to
+  // ADC12_A control registers
+  ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON; // Internal ref = 1.5V
+  ADC12CTL1 = ADC12SHP; // Enable sample timer
+  // Using ADC12MEM0 to store reading
+  ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10; // ADC i/p ch A10 = temp sense
+  // ACD12SREF_1 = internal ref = 1.5v
+  __delay_cycles(100); // delay to allow Ref to settle
+  ADC12CTL0 |= ADC12ENC; // Enable conversion
+  // Use calibration data stored in info memory
+  bits30 = CALADC12_15V_30C;
+  bits85 = CALADC12_15V_85C;
+  degC_per_bit = ((float)(85.0 - 30.0))/((float)(bits85-bits30));
+  /*----------------------------------------------------------------------------------- */
 
   // Initialize the MSP430
   initLeds();
@@ -28,7 +63,24 @@ void main() {
   //Start the A2 timer
   runtimerA2();
   while (1) {
-    //You can wrap this in a function but I'm pretty sure it needs to return a pointer to an array to work properly, so you'd need to initialize the array and allocate memory first
+  /*----------------------------------------------------------------------------------- */
+    ADC12CTL0 &= ~ADC12SC; // clear the start bit
+    ADC12CTL0 |= ADC12SC; // Sampling and conversion start
+    // Single conversion (single channel)
+    // Poll busy bit waiting for conversion to complete
+    while (ADC12CTL1 & ADC12BUSY)
+      __no_operation();
+    in_temp = ADC12MEM0; // Read in results if conversion
+    // Temperature in Celsius. See the Device Descriptor Table section in the
+    // System Resets, Interrupts, and Operating Modes, System Control Module
+    // chapter in the device user's guide for background information on the
+    // used formula.
+    temperatureDegC = (float)((long)in_temp - CALADC12_15V_30C) * degC_per_bit +30.0;
+    // Temperature in Fahrenheit Tf = (9/5)*Tc + 32
+    temperatureDegF = temperatureDegC * 9.0/5.0 + 32.0;
+    __no_operation();
+  /*----------------------------------------------------------------------------------- */
+  //You can wrap this in a function but I'm pretty sure it needs to return a pointer to an array to work properly, so you'd need to initialize the array and allocate memory first
     //Stores month abbreviations  as an array of char in an array
     const char* month_abbr[] = {
       "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
@@ -38,8 +90,9 @@ void main() {
       31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
     };
         unsigned int month = 0;
-        unsigned int days = global_counter / 86400;
-        for (int i = 0; i < 12; i++) {
+        unsigned int day = global_counter / 86400;
+        int i;
+        for (i = 0; i < 12; i++) {
           if (day <= month_days[i]) {
             month = i;
             break;
@@ -77,9 +130,7 @@ void main() {
         disp_time[7] = seconds_ones;
         disp_time[8] = '\0';
         // Write to the display
-        Graphics_clearDisplay(&g_sContext); // Clear the display
         Graphics_drawStringCentered(&g_sContext, disp_date, 7, 48, 15, TRANSPARENT_TEXT);
         Graphics_drawStringCentered(&g_sContext, disp_time, 9, 48, 35, TRANSPARENT_TEXT);
-        Graphics_flushBuffer(&g_sContext);
   }
 }
