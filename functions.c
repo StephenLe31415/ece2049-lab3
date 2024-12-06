a2b0dac60e5932ee94b3ec89a8ae68dbcdd4ad30
Stephen Le <huy.lvsong@gmail.com>
2024-12-06 15:52:20 -0500

    Abstracted ADC12 setup.

--- functions.c
+++ functions.c
@@ -1,6 +1,129 @@
 #include "functions.h"
 // User written functions
 
+//Global timer A2 for clock, frequency is 32768 with an /8 division interrupt timing is 1 second.
+void runtimerA2(void)
+{
+  TA2CTL = TASSEL_1 | MC_1 | ID_3;
+  TA2CCR0 = 4095;
+  TA2CCTL0 = CCIE;
+}
+
+// Config ADC12
+void config_ADC(void) {
+  //Set Port P8.0 (the slider) to digital I/O mode
+  P8OUT |= BIT0;
+
+  // TODO The ADC needs to read sequentially from the temp sensor, then the slider. It needs to read in from INCH_5 to a MCTL register with a VREF of 5V
+  REFCTL0 &= ~REFMSTR; // Reset REFMSTR to hand over control of
+  // internal reference voltages to
+  // ADC12_A control registers
+  ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON; // Internal ref = 1.5V
+  ADC12CTL1 = ADC12SHP + ADC12CONSEQ_1; // Enable sample timer and set sequential mode
+  // Using ADC12MEM0 to store reading
+  ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10; // ADC i/p ch A10 = temp sense
+  // Slider stored in to MCTL1 5v reference VCC -> VSS
+  ADC12MCTL1 = ADC12SREF_0 + ADC12INCH_5 + ADC12EOS;
+  // ACD12SREF_1 = internal ref = 1.5v
+  __delay_cycles(100); // delay to allow Ref to settle
+  ADC12CTL0 |= ADC12ENC; // Enable conversion
+  // Use calibration data stored in info memory
+  bits30 = CALADC12_15V_30C;
+  bits85 = CALADC12_15V_85C;
+  degC_per_bit = ((float)(85.0 - 30.0))/((float)(bits85-bits30));
+}
+
+// Display date
+void displayDate(char* date, volatile long unsigned int globalTime) {
+  const char* month_abbr[] = {
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  //Stores length of month to be used to decrement days later
+  const int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+  unsigned int month = 0;
+  unsigned int day = global_counter / 86400;
+  for (int i = 0; i < 12; i++) {
+    if (day <= month_days[i]) {
+      month = i;
+      break;
+    }
+    day = day - month_days[i];
+  }
+  char day_tens = ((day - (day % 10)) / 10) + '0';
+  char day_ones = (day % 10) + '0';
+
+  date[0] = month_abbr[month][0];
+  date[1] = month_abbr[month][1];
+  date[2] = month_abbr[month][2];
+  date[3] = ' ';
+  date[4] = day_tens;
+  date[5] = day_ones;
+  date[6] = '\0';
+
+  Graphics_drawStringCentered(&g_sContext, date, 7, 48, 15, TRANSPARENT_TEXT);
+}
+
+// Display time
+void displayTime(char* disp_time, volatile long unsigned int globalTime) {
+  unsigned int hours = (global_counter / 3600) % 24;
+  unsigned int minutes = (global_counter / 60) % 60;
+  unsigned int seconds = global_counter % 60;
+  char hours_tens = ((hours - (hours % 10)) / 10) + '0';
+  char hours_ones = (hours % 10) + '0';
+  char minutes_tens = ((minutes - (minutes % 10)) / 10) + '0';
+  char minutes_ones = (minutes % 10) + '0';
+  char seconds_tens = ((seconds - (seconds % 10)) / 10) + '0';
+  char seconds_ones = (seconds % 10) + '0';
+
+  disp_time[0] = hours_tens;
+  disp_time[1] = hours_ones;
+  disp_time[2] = ':';
+  disp_time[3] = minutes_tens;
+  disp_time[4] = minutes_ones;
+  disp_time[5] = ':';
+  disp_time[6] = seconds_tens;
+  disp_time[7] = seconds_ones;
+  disp_time[8] = '\0';
+
+  Graphics_drawStringCentered(&g_sContext, disp_time, 9, 48, 35, TRANSPARENT_TEXT);
+}
+
+// Display temp in C
+void displayTempC(char* disp_c, float temperatureDegC) {
+  temperatureDegC = temperatureDegC * 10;
+  unsigned int int_degC = (unsigned int)temperatureDegC;
+  char c_tens = ((int_degC / 100) % 10) + '0';
+  char c_ones = ((int_degC / 10) % 10) + '0';
+  char c_tenths = (int_degC % 10) + '0';
+
+  disp_c[0] = c_tens;
+  disp_c[1] = c_ones;
+  disp_c[2] = '.';
+  disp_c[3] = c_tenths;
+  disp_c[4] = ' ';
+  disp_c[5] = 'C';
+
+  Graphics_drawStringCentered(&g_sContext, disp_c, 6, 48, 45, TRANSPARENT_TEXT);
+}
+
+// Display temp in F
+void displayTempF(char* tempF, float temperatureDegF) {
+  temperatureDegF = temperatureDegF * 10;
+  unsigned int int_degF = (unsigned int)temperatureDegF;
+  char f_tens = ((int_degF / 100) % 10) + '0';
+  char f_ones = ((int_degF / 10) % 10) + '0';
+  char f_tenths = (int_degF % 10) + '0';
+  
+  tempF[0] = f_tens;
+  tempF[1] = f_ones;
+  tempF[2] = '.';
+  tempF[3] = f_tenths;
+  tempF[4] = ' ';
+  tempF[5] = 'F';
+
+  Graphics_drawStringCentered(&g_sContext, tempF, 6, 48, 55, TRANSPARENT_TEXT);
+}
+
+/***************************************************************************************************************************************** */
 // Initializes the two user LEDs
 void init_user_leds()
 {
@@ -58,98 +181,4 @@ unsigned int read_buttons() {
   pressed |= BIT2 & ~(P2IN & BIT2);
   pressed |= BIT3 & ~((P7IN & BIT4) >> 1);
   return pressed;
-}
-
-//Global timer A2 for clock, frequency is 32768 with an /8 division interrupt timing is 1 second.
-void runtimerA2(void)
-{
-  TA2CTL = TASSEL_1 | MC_1 | ID_3;
-  TA2CCR0 = 4095;
-  TA2CCTL0 = CCIE;
-}
-
-void displayDate(char* date, volatile long unsigned int globalTime) {
-  const char* month_abbr[] = {
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-  //Stores length of month to be used to decrement days later
-  const int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-  unsigned int month = 0;
-  unsigned int day = global_counter / 86400;
-  for (int i = 0; i < 12; i++) {
-    if (day <= month_days[i]) {
-      month = i;
-      break;
-    }
-    day = day - month_days[i];
-  }
-  char day_tens = ((day - (day % 10)) / 10) + '0';
-  char day_ones = (day % 10) + '0';
-
-  date[0] = month_abbr[month][0];
-  date[1] = month_abbr[month][1];
-  date[2] = month_abbr[month][2];
-  date[3] = ' ';
-  date[4] = day_tens;
-  date[5] = day_ones;
-  date[6] = '\0';
-
-  Graphics_drawStringCentered(&g_sContext, date, 7, 48, 15, TRANSPARENT_TEXT);
-}
-
-void displayTime(char* disp_time, volatile long unsigned int globalTime) {
-  unsigned int hours = (global_counter / 3600) % 24;
-  unsigned int minutes = (global_counter / 60) % 60;
-  unsigned int seconds = global_counter % 60;
-  char hours_tens = ((hours - (hours % 10)) / 10) + '0';
-  char hours_ones = (hours % 10) + '0';
-  char minutes_tens = ((minutes - (minutes % 10)) / 10) + '0';
-  char minutes_ones = (minutes % 10) + '0';
-  char seconds_tens = ((seconds - (seconds % 10)) / 10) + '0';
-  char seconds_ones = (seconds % 10) + '0';
-
-  disp_time[0] = hours_tens;
-  disp_time[1] = hours_ones;
-  disp_time[2] = ':';
-  disp_time[3] = minutes_tens;
-  disp_time[4] = minutes_ones;
-  disp_time[5] = ':';
-  disp_time[6] = seconds_tens;
-  disp_time[7] = seconds_ones;
-  disp_time[8] = '\0';
-
-  Graphics_drawStringCentered(&g_sContext, disp_time, 9, 48, 35, TRANSPARENT_TEXT);
-}
-
-void displayTempC(char* disp_c, float temperatureDegC) {
-  temperatureDegC = temperatureDegC * 10;
-  unsigned int int_degC = (unsigned int)temperatureDegC;
-  char c_tens = ((int_degC / 100) % 10) + '0';
-  char c_ones = ((int_degC / 10) % 10) + '0';
-  char c_tenths = (int_degC % 10) + '0';
-
-  disp_c[0] = c_tens;
-  disp_c[1] = c_ones;
-  disp_c[2] = '.';
-  disp_c[3] = c_tenths;
-  disp_c[4] = ' ';
-  disp_c[5] = 'C';
-
-  Graphics_drawStringCentered(&g_sContext, disp_c, 6, 48, 45, TRANSPARENT_TEXT);
-}
-
-void displayTempF(char* tempF, float temperatureDegF) {
-  temperatureDegF = temperatureDegF * 10;
-  unsigned int int_degF = (unsigned int)temperatureDegF;
-  char f_tens = ((int_degF / 100) % 10) + '0';
-  char f_ones = ((int_degF / 10) % 10) + '0';
-  char f_tenths = (int_degF % 10) + '0';
-  
-  tempF[0] = f_tens;
-  tempF[1] = f_ones;
-  tempF[2] = '.';
-  tempF[3] = f_tenths;
-  tempF[4] = ' ';
-  tempF[5] = 'F';
-
-  Graphics_drawStringCentered(&g_sContext, tempF, 6, 48, 55, TRANSPARENT_TEXT);
 }
--- main.c
+++ main.c
@@ -21,7 +21,6 @@ __interrupt void timer_a2() {
 }
 
 void main() {
-
   /*----------------------------------------------------------------------------------- */
   volatile float temperatureDegC;
   volatile float temperatureDegF;
@@ -35,28 +34,8 @@ void main() {
   // Global interrupt enable
   _BIS_SR(GIE);
 
-  //Set Port P8.0 (the slider) to digital I/O mode
-  P8OUT |= BIT0;
-
-  /*----------------------------------------------------------------------------------- */
-  // TODO The ADC needs to read sequentially from the temp sensor, then the slider. It needs to read in from INCH_5 to a MCTL register with a VREF of 5V
-  REFCTL0 &= ~REFMSTR; // Reset REFMSTR to hand over control of
-  // internal reference voltages to
-  // ADC12_A control registers
-  ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON; // Internal ref = 1.5V
-  ADC12CTL1 = ADC12SHP + ADC12CONSEQ_1; // Enable sample timer and set sequential mode
-  // Using ADC12MEM0 to store reading
-  ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10; // ADC i/p ch A10 = temp sense
-  // Slider stored in to MCTL1 5v reference VCC -> VSS
-  ADC12MCTL1 = ADC12SREF_0 + ADC12INCH_5 + ADC12EOS;
-  // ACD12SREF_1 = internal ref = 1.5v
-  __delay_cycles(100); // delay to allow Ref to settle
-  ADC12CTL0 |= ADC12ENC; // Enable conversion
-  // Use calibration data stored in info memory
-  bits30 = CALADC12_15V_30C;
-  bits85 = CALADC12_15V_85C;
-  degC_per_bit = ((float)(85.0 - 30.0))/((float)(bits85-bits30));
-  /*----------------------------------------------------------------------------------- */
+  // Configure the ADC12
+  config_ADC();
 
   // Initialize the MSP430
   initLeds();
@@ -77,22 +56,23 @@ void main() {
     while (ADC12CTL1 & ADC12BUSY)
       __no_operation();
     in_temp = ADC12MEM0; // Read in results if conversion
-    // Temperature in Celsius. See the Device Descriptor Table section in the
-    // System Resets, Interrupts, and Operating Modes, System Control Module
-    // chapter in the device user's guide for background information on the
-    // used formula.
     temperatureDegC = (float)((long)in_temp - CALADC12_15V_30C) * degC_per_bit +30.0;
     // Temperature in Fahrenheit Tf = (9/5)*Tc + 32
     temperatureDegF = temperatureDegC * 9.0/5.0 + 32.0;
     //Set store the slider value in ADC12MEM1 in slider
     slider = ADC12MEM1;
   /*----------------------------------------------------------------------------------- */
+    
+    // Display stuff
     char date[7] = {0};
     char time[9] = {0};
     char tempC[7] = {0};
     char tempF[7] = [0];
+    displayDate(date, global_counter);
+    displayTime(time, global_counter);
+    displayTempC(tempC, temperatureDegC);
+    displayTempF(tempF, temperatureDegF);
   
-    // hello
-    // Hello this is a second test
+    
   }
 }
