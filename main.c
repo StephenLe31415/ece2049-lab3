583027d2aa15716d6c34b41af2346ce760f669f6
rhogan <rhogan@wpi.edu>
2024-12-10 17:28:19 -0500

    Fixed switch case rollover logic

--- main.c
+++ main.c
@@ -174,7 +174,7 @@ void main() {
           user_input = 0;
           while (user_input == 0 && temp_counter == global_counter)
             user_input = read_launchpad_button();
-          num_pressed += (user_input % 5); // Wrap around "Month - Date - Hour - Min - Sec" logic
+          num_pressed = (num_pressed + user_input) % 5; // Wrap around "Month - Date - Hour - Min - Sec" logic
           // Traversing logic
           switch (num_pressed) {
             case 1: { //MONTH
